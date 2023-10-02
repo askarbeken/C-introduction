d02f693f8e3f519349a163f1dc59479d7166adb4
Askar <abekenov@mail.ru>
2023-10-03 00:44:56 +0600

    commit2

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+Console.Write("Введите трёхзначное число: ");
+string input = Console.ReadLine();
+if (input.Length == 3 && input [0] >= '0' && input [1] <= '9' && input[1] >= '0' && input[1] <= '9' && input[2] >= '0' && input[2] <= '9')
+{
+    int secondDigit = (input[1] - '0');
+    Console.WriteLine($"Вторая цифра числа: {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Неправильно! Введите трёхзначное число, пожалуйста");
+}
---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length >= 3)
+{
+    string thirdDigit = new string(number[2], 1);
+    Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
